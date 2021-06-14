1cef127a8f0d08eb88add3ea192e1e66b4a254b9
0xJavier <0xJMunoz@gmail.com>
2021-06-14 15:30:38 -0400

    initial commit

---
+++ NTBank/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NTBank
+//
+//  Created by Javier Munoz on 6/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Loaded")
+    }
+
+
+}
+
