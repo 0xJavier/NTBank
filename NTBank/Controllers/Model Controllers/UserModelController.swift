9a5876cf6e536436ade50a0b8f1c4bf79587e2e3
0xJavier <0xJMunoz@gmail.com>
2021-07-01 18:01:28 -0400

    update user model controller

--- NTBank/Controllers/Model Controllers/UserModelController.swift
+++ NTBank/Controllers/Model Controllers/UserModelController.swift
@@ -7,6 +7,27 @@
 
 import Foundation
 
+protocol UserModelControllerDelegate: AnyObject {
+    func didFetchUser(user: User)
+    func didFetchTransactions(transactions: [Transaction])
+}
+
 class UserModelController {
-    private let logicController = UserLogicController()
+    public private(set) var user = UserMockData().user
+    public private(set) var transactions = UserMockData().transactions
+    
+    weak var userDelegate: UserModelControllerDelegate?
+    
+    init() {
+        retrieveUser()
+        retrieveTransactions()
+    }
+    
+    func retrieveUser() {
+        self.userDelegate?.didFetchUser(user: self.user)
+    }
+    
+    func retrieveTransactions() {
+        self.userDelegate?.didFetchTransactions(transactions: self.transactions)
+    }
 }
