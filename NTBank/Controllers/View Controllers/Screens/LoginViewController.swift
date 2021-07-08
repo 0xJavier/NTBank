7d49866608b47a124b9b5908c107751436e8e441
0xJavier <0xJMunoz@gmail.com>
2021-07-08 19:44:56 -0400

    add firestore via spm + add login logic
    App now connects to firestore and can authentication users in LoginVC.

--- NTBank/Controllers/View Controllers/Screens/LoginViewController.swift
+++ NTBank/Controllers/View Controllers/Screens/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController, LoginInterfaceViewDelegate {
     
@@ -15,6 +16,7 @@ class LoginViewController: UIViewController, LoginInterfaceViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        createDismissKeyboardTapGesture()
         loginInterface.loginDelegate = self
     }
     
@@ -30,12 +32,74 @@ class LoginViewController: UIViewController, LoginInterfaceViewDelegate {
     
     //MARK: - LoginScreenViewControllerDelegate
     func didSelectLoginButton() {
-        let tabview = MainTabViewController()
-        UIApplication.shared.windows.first?.rootViewController = tabview
-        UIApplication.shared.windows.first?.makeKeyAndVisible()
+        guard let email = loginInterface.emailTextfield.text else {
+            print("LOG: Could not unwrap email text string")
+            return
+        }
+        
+        guard let password = loginInterface.passwordTextfield.text else {
+            print("LOG: Could not unwrap password text string")
+            return
+        }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] authDataResult, error in
+            guard let self = self else { return }
+            if let error = error {
+                print("ERROR: \(error.localizedDescription)")
+            } else {
+                let tabview = MainTabViewController()
+                tabview.modalPresentationStyle = .fullScreen
+                self.present(tabview, animated: true)
+            }
+        }
     }
     
     func didSelectForgotPasswordButton() {
+        let title = "Forgot Password?"
+        let message = "Please enter your email to recieve a reset link."
         
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alertController.addTextField()
+        alertController.textFields![0].keyboardType = .emailAddress
+        
+        let saveAction = UIAlertAction(title: "Send", style: .default) { action in
+            guard let email = alertController.textFields![0].text else { return }
+            self.sendResetPasswordLink(with: email)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { action in
+            return
+        }
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(saveAction)
+        
+        DispatchQueue.main.async {
+            self.present(alertController, animated: true)
+        }
+    }
+    
+    func sendResetPasswordLink(with email: String) {
+        Auth.auth().sendPasswordReset(withEmail: email) { error in
+            if let error = error {
+                print("LOG: \(error.localizedDescription)")
+                return
+            } else {
+                let title = "Success!"
+                let message = "Successfully sent a reset link."
+                
+                let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+                
+                let saveAction = UIAlertAction(title: "Ok", style: .default) { _ in return }
+                
+                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in return }
+                
+                alertController.addAction(cancelAction)
+                alertController.addAction(saveAction)
+                
+                DispatchQueue.main.async { self.present(alertController, animated: true) }
+            }
+        }
     }
 }
